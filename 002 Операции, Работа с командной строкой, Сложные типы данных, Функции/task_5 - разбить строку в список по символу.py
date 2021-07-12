527d5d1caa48e63492c5fb48fd4f9dc2dbcd18ee
Sergei Loginov <lsl330470@gmail.com>
2021-07-12 21:29:42 +0600

    Задача решена.

--- 002 Операции, Работа с командной строкой, Сложные типы данных, Функции/task_5 - разбить строку в список по символу.py
+++ 002 Операции, Работа с командной строкой, Сложные типы данных, Функции/task_5 - разбить строку в список по символу.py
@@ -2,6 +2,7 @@
 Взять строку '/bin:/usr/bin:/usr/local/bin' и разбить
 ее в список по символу ':'
 """
+
 s = '/bin:/usr/bin:/usr/local/bin'
 
 print(s.split(':'))  # ['/bin', '/usr/bin', '/usr/local/bin']
