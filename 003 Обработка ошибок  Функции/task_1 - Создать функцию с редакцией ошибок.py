22dc78048bb5ed9d96bfeb970e754b553f77cc72
Sergei Loginov <lsl330470@gmail.com>
2021-07-13 21:29:58 +0600

    task_2 решена.

---
+++ 003 Обработка ошибок  Функции/task_1 - Создать функцию с редакцией ошибок.py
@@ -0,0 +1,29 @@
+"""
+Пользователь вводит число, если оно четное -
+выбрасываем исключение ValueError, если оно
+меньше 0 - TypeError, если оно больше 10 - IndexError.
+Обрабатываем ошибку, говорим пользователю, в чем его ошибка/
+"""
+
+
+def result(i):
+    if i % 2 == 0:
+        raise ValueError
+    elif i < 0:
+        raise TypeError
+    elif i > 10:
+        raise IndexError
+
+for i in [-1,2,11]:
+    try:
+        result(i)
+    except ValueError as e:
+        print('Возникла ошибка значения', e)
+    except TypeError as e:
+        print('Возникла ошибка типа', e)
+    except IndexError as e:
+        print('Возникла ошибка индекса', e)
+
+
+a = int(input())
+print(result(a))
