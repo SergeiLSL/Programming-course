6ba82b505abcf49caa70e0245bdf112ec8bb80a9
Sergei Loginov <lsl330470@gmail.com>
2021-07-15 17:51:32 +0600

    Подсчет итоговых ответов.py - доработал ввод, взять на заметку.

--- Глава 2 - Операции, Работа с командной строкой, Сложные типы данных, Функции/Примеры решения задач/Подсчет итоговых ответов.py
+++ Глава 2 - Операции, Работа с командной строкой, Сложные типы данных, Функции/Примеры решения задач/Подсчет итоговых ответов.py
@@ -2,29 +2,28 @@
 def P3():
     q, e = 0, 0
     import random
-    for _ in range(20):
+    for _ in range(5):
         a = int(random.randint(1, 30))
         b = random.randint(1, 30)
         c = random.choice('+''-')
 
-        i = a, c, b
-        text = input(i)
+        text = input(f'{a} {c} {b} = ')
         if str(c) == '+':
-            if (int(a) + int(b)) == int(text):
+            if int(a) + int(b) == int(text):
                 print("Верно")
                 q += 1
             else:
                 print("Неверно")
                 e += 1
         if str(c) == "-":
-            if (int(a) - int(b)) == int(text):
+            if int(a) - int(b) == int(text):
                 print("Верно")
                 q += 1
             else:
                 print("Неверно")
                 e += 1
 
-    print("\nВерно:{}\nНеверно:{}".format(q, e))
+    print("\nВерно: {}\nНеверно: {}".format(q, e))
 
 
 if __name__ == '__main__':
