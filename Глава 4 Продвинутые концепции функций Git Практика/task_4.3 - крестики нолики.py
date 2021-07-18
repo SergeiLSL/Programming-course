068d9d5f0ab30ecd4a7c9f75bdc84f5285b45679
Sergei Student <lsl1959@mail.ru>
2021-07-18 21:36:24 +0600

    task_4.3 - записал условие

---
+++ Глава 4 Продвинутые концепции функций Git Практика/task_4.3 - крестики нолики.py
@@ -0,0 +1,14 @@
+"""
+Крестики нолики
+https://all-python.ru/primery/krestiki-noliki.html
+https://it-black.ru/igra-krestiki-noliki-dlja-dvuh-igrokov-na-python/
+
+Написать программу для игры в крестики нолики
+
+
+Калькулятор
+https://all-python.ru/primery/kalkulyator.html
+
+Календарь
+https://all-python.ru/primery/kalendar.html
+"""
