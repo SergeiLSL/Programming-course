7363fd9c7e79d71483a1c39e4ef0fa4a70333b51
Sergei Student <lsl1959@mail.ru>
2021-07-19 19:17:56 +0600

    task_4.3.0 - создадим массив для хранения состояния на поле

--- Глава 4 Продвинутые концепции функций Git Практика/task_4.3.0 - как создать игровое поле.py
+++ Глава 4 Продвинутые концепции функций Git Практика/task_4.3.0 - как создать игровое поле.py
@@ -74,6 +74,37 @@ import sys
 """
 Научимся реагировать на мышку
 """
+# pygame.init()
+#
+# size = (510, 510)  # размер игрового поля
+# screen = pygame.display.set_mode(size)  # выводим на экран игровое поля
+# pygame.display.set_caption('Game field')  # название игрового поля
+#
+# # зададим параметры квадратика
+# width = height = 40  # ширина и высота квадратика
+# red = (255, 0, 0)  # цвет квадратика
+# margin = 10  # для того чтобы межу квадратиками был промежуток
+#
+# while True:
+#     for event in pygame.event.get():  # цикл обработки событий
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#         #  добавим блок - если у события был тип : нажатие на мышку,
+#         #  то заводим две переменные, определяющие координаты этого события
+#         elif event.type == pygame.MOUSEBUTTONDOWN:
+#             x_mouse, y_mouse = pygame.mouse.get_pos()
+#             print(f'x = {x_mouse} y = {y_mouse}')
+#     for col in range(10):  # надо заполнить первую строку
+#         for row in range(10):
+#             x = col * width + (col + 1) * margin  # меняется только координата x
+#             y = row * height + (row + 1) * margin  #
+#             pygame.draw.rect(screen, red, (x, y, width, height))
+#     pygame.display.update()
+
+"""
+Создадим массив, в которм будем хранить состояние на поле
+"""
 pygame.init()
 
 size = (510, 510)  # размер игрового поля
@@ -82,14 +113,20 @@ pygame.display.set_caption('Game field')  # название игрового п
 
 # зададим параметры квадратика
 width = height = 40  # ширина и высота квадратика
-red = (255, 0, 0)  # цвет квадратика
 margin = 10  # для того чтобы межу квадратиками был промежуток
+red = (255, 0, 0)  # цвет квадратика
+
 
 while True:
     for event in pygame.event.get():  # цикл обработки событий
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        #  добавим блок - если у события был тип : нажатие на мышку,
+        #  то заводим две переменные, определяющие координаты этого события
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            print(f'x = {x_mouse} y = {y_mouse}')
     for col in range(10):  # надо заполнить первую строку
         for row in range(10):
             x = col * width + (col + 1) * margin  # меняется только координата x
