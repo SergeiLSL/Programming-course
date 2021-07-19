ba3d2a57ff571b1f364e079598560582db60dd8c
Sergei Student <lsl1959@mail.ru>
2021-07-19 14:33:24 +0600

    task_4.2.2 - все получилось. сплошные функции.

--- Глава 4 Продвинутые концепции функций Git Практика/task_4.2.1 - виселица.py
+++ Глава 4 Продвинутые концепции функций Git Практика/task_4.2.1 - виселица.py
@@ -28,9 +28,37 @@ def is_alpha(letter):
         return is_alpha(input())
 
 
+def play_again(answer):
+    """ Функция повтора игры """
+    if answer.lower() == 'д':
+        return play_start(), True
+    else:
+        print('До встречи.')
+
+
+def play_start():
+    """ Функция начала игры """
+    global count, n, word, task_word
+    words = ['Животное', 'крокодил', 'птица', 'Пирамида', 'треугольник', 'мячик',
+             'Обруч', 'Скакалка', 'Мама', 'окно', 'Магазин', 'стол', 'машина',
+             'шкаф', 'ручка', 'Молоко', 'телевизор', 'телефон', 'Париж']
+
+    word = random.choice(words).lower()  # выбор случайного слова из списка
+    # print(word)
+    task_word = []
+    for b in word:
+        task_word += ['_']  # замена букв в слове на нижний пробел
+    print(*task_word)
+    n = len(word)
+    print(attempt(n))  # определяем количество попыток
+    count = 0
+    print('Введите букву: ')
+    play_gallows()  # вызываем игру
+
+
 def play_gallows():
     """ Функция игры """
-    global count, n
+    global count, n, task_word
     while True:
         if count == len(word):
             print('Вы спасли себе жизнь!')
@@ -41,14 +69,14 @@ def play_gallows():
         elif ''.join(task_word).isalpha():
             print('Вы спасли себе жизнь!')
             break
-        user_letter = is_alpha(input())
+        user_letter = is_alpha(input())  # вводим букву и определяем правильность ввода
         n -= 1
         if user_letter in word:
             print(user_letter)
             for i in range(len(task_word)):
                 s = len(task_word)
                 a = -1
-                while s != 0:
+                while s != 0:  # если введенная буква есть в загаданном слове, вставляем
                     a = word.find(user_letter, a + 1)
                     if a == -1:
                         break
@@ -65,26 +93,13 @@ def play_gallows():
 
 print("*" * 3, " Игра 'Виселица' ", "*" * 3)
 name = input('Пожалуйста введите ваше имя: ')
+play_start()
+a = True
+while a:
+    print('Хотите сыграть ещё раз? (д/н)')
+    a = play_again(input())
 
-words = ['Животное', 'крокодил', 'птица', 'Пирамида', 'треугольник', 'мячик',
-         'Обруч', 'Скакалка', 'Мама', 'окно', 'Магазин', 'стол', 'машина',
-         'шкаф', 'ручка', 'Молоко', 'телевизор', 'телефон', 'Париж']
 
-word = random.choice(words).lower()
-print(word)
-
-task_word = []
-for b in word:
-    task_word += ['_']
-
-print(*task_word)
-
-n = len(word)
-print(attempt(n))
-count = 0
-
-print('Введите букву: ')
-play_gallows()
 
 
 
