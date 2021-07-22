509f1a67ae56dd20621269099ceb2e473a869f03
Sergei Student <lsl1959@mail.ru>
2021-07-22 20:47:10 +0600

    Добавил для изучения

--- Глава 5 - Объектно-ориентированное программирование/Class Work/polymorphism - полиморфизм/polymorphism.py
+++ Глава 5 - Объектно-ориентированное программирование/Class Work/polymorphism - полиморфизм/polymorphism.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+"""
+Полиморфизм
+Позволяет использовать функции по разному, вне
+зависимости от типа их параметров
+"""
+
+
+def sum_two_objects(one, two):
+    return one + two
+
+
+print(sum_two_objects(1, 2))  # 3 - если числа, то складываем
+print(sum_two_objects('ab', 'cd'))  # abcd - если строки, то контагенацию
+print(sum_two_objects(['a', 'b'], ['c', 'd']))  # ['a', 'b', 'c', 'd'] - объединение множеств
+print(sum_two_objects(('a', 'b'), ('c', 'd')))  # ('a', 'b', 'c', 'd') - объединение множеств
+
+# ========================================================
 
 
 class Parent(object):
@@ -20,12 +37,10 @@ def call_obj(obj):
     obj.call()
 
 
-def sum_two_objects(one, two):
-    return one + two
+call_obj(Child())  # Child
+call_obj(Parent())  # Parent
+call_obj(Example())  # Ex
 
-
-call_obj(Child())
-call_obj(Parent())
 
 
 
