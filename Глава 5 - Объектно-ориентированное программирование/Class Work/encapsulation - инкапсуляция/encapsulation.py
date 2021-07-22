0a61e6d5ca0186b47b4b9a644c719d3fa40507c5
Sergei Student <lsl1959@mail.ru>
2021-07-22 20:20:01 +0600

    Добавил для изучения

--- Глава 5 - Объектно-ориентированное программирование/Class Work/encapsulation - инкапсуляция/encapsulation.py
+++ Глава 5 - Объектно-ориентированное программирование/Class Work/encapsulation - инкапсуляция/encapsulation.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
+# Обратить внимание на подчеркивание!!!!
+
 
 class Example(object):
     def __init__(self):
-        self.a = 1
-        self._b = 2
-        self.__c = 3
-        print('{} {} {}'.format(
-            self.a, self._b, self.__c))
+        self.a = 1  # полностью доступно, можно использовать в любой точке, где угодно
+        self._b = 2  # доступно, но лучше не трогать (сигнал для разработчиков)
+        self.__c = 3  # недоступно, доступ обеспечивается определённым образом
+        print('{} {} {}'.format(self.a, self._b, self.__c))  # 1 2 3
+        # внутри класса можно использовать все три поля
+        # вне класса можем использовать а и _b
 
     def call(self):
         print('Called!')
@@ -25,13 +28,19 @@ class Inh(Example):
         self.__c = 6
 
 
-
 example = Example()
-print(example.a)
-print(example._b)
+print(example.a)  # 1
+print(example._b)  # 2
 
-
+# При обращении к с выдает ошибку и пишет, что это не атрибут 'Example'
 try:
-    print(example.__c)
+    print(example.__c)  # 'Example' object has no attribute '__c'
 except AttributeError as ex:
-    print(ex)
+    print(ex)  # 'Example' object has no attribute '__c'
+
+"""
+Три типа имен
+1. a - полностью доступно, можно использовать в любой точке, где угодно
+2. _b - доступно, но лучше не трогать (сигнал для разработчиков)
+3. __c - недоступно, доступ обеспечивается определённым образом
+"""
