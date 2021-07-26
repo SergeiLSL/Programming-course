0d3434cbe9bff7287825071485b277ac4c755980
Sergei Student <lsl1959@mail.ru>
2021-07-26 18:51:52 +0600

    Для изучения_

--- Глава 6 - Классы и обьекты/Work of class - 6/magic_methods/list_methods.py
+++ Глава 6 - Классы и обьекты/Work of class - 6/magic_methods/list_methods.py
@@ -3,9 +3,10 @@
 Функциональность словаря
 """
 
+
 class DictFunctionality(object):
     def __init__(self, values=None):
-        if values is None:
+        if values is None:  # is обозначет ==
             self.values = {}
         elif isinstance(values, dict):
             self.values = values
@@ -14,37 +15,62 @@ class DictFunctionality(object):
 
     # Converting to string:
     def __str__(self):
-        return str(self.values) if self.values else ''
+        if self.values:
+            return str(self.values)
+        else:
+            return ''
+        # return str(self.values) if self.values else ''  # тоже самое
 
-    # Items management:
-    def __getitem__(self, key):
+    # Items management: (методы словаря)
+    def __getitem__(self, key):  # возвращают значение словаря
         return self.values[key]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key, value):  # назначают значение словаря
         self.values[key] = value
 
-    def __delitem__(self, key):
+    def __delitem__(self, key):  # удаляют значение словаря
         del self.values[key]
 
-    # Iteration:
+    # Iteration:  #
     def __iter__(self):
         return iter(self.values)
 
-    # `in` operation:
+    # `in` operation:  # поиск элемента словаря
     def __contains__(self, item):
         return item in self.values
 
-    def __len__(self):
+    def __len__(self):  # получени длины словаря
         return len(self.values)  # self.values.__len__()
 
 
 if __name__ == '__main__':
-    l = DictFunctionality({'1key': 'some_value'})
-    l[1] = 'item1'
-    print(str(l), l[1])
+    l = DictFunctionality({'1key': 'some_value'})  # сработал метод __init__
+    l[1] = 'item1'  # сработал метод __setitem__
+    print(str(l), l[1])  # сработал метод __str__, __delitem__
 
     for item in l:
-        print(item, l[item])
+        print(item, l[item])  # сработал метод __iter__
 
-    print('s' in l, 1 in l)
-    print(len(l))
+    print('s' in l, 1 in l)  # сработал метод __contains__
+    print(len(l))  # сработал метод __len__
+
+"""
+Полезное свойство
+"""
+
+
+class MathObject:
+    def __init__(self):
+        self.value = 2
+        self.other = 3
+
+    def j(self):
+        pass
+
+
+t = MathObject()
+
+print(t.__dict__)  # {'value': 2, 'other': 3}
+
+print(dir(t))
+
